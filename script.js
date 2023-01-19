5ac05635bb3ad02c0b6314f34e039f8ad52af7fd
Yurii Serediuk <yserediu@cisco.com>
2023-01-19 18:05:47 +0200

    Changed elements

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ dragula([
 document.getElementById('1'),
 document.getElementById('2'),
 document.getElementById('3'),
-document.getElementById('4'),
-document.getElementById('5')]).
+document.getElementById('4')]).
 
 
 on('drag', function (el) {
